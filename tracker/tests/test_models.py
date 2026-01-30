ff5998cc90a0255279007afbd10209f749b42569
Mike Melnikov <mikemelnikov@Mikes-MacBook-Air.local>
2026-01-30 14:59:02 -0600

    Tests for transaction and income creation were added

--- tracker/tests/test_models.py
+++ tracker/tests/test_models.py
@@ -3,6 +3,34 @@ from django.core.exceptions import ValidationError
 from django.contrib.auth.models import User
 from tracker.models import Transaction, Income
 
+
+@pytest.mark.django_db
+def test_transaction_creation():
+    user = User.objects.create_user(username='testuser', password='testpass123')
+    transaction = Transaction.objects.create(
+        slug='test-transaction',
+        store='Test Store',
+        amount=100.50,
+        category=Transaction.CATEGORY_GROCERIES,
+        owner=user
+    )
+    assert transaction.store == 'Test Store'
+    assert transaction.amount == 100.50
+    assert transaction.category == Transaction.CATEGORY_GROCERIES
+    assert transaction.owner == user 
+
+@pytest.mark.django_db
+def test_income_creation():
+    user = User.objects.create_user(username='testuser2', password='testpass123')
+    income = Income.objects.create(
+        source='Test Source',
+        amount=250.75,
+        owner=user
+    )
+    assert income.source == 'Test Source'
+    assert income.amount == 250.75
+    assert income.owner == user
+
 @pytest.mark.django_db #DATABASE ACCESS
 def test_transaction_negative_amount():
     user = User.objects.create_user(username='testuser', password='testpass123')
